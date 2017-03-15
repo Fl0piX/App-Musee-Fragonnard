69a59f5fa2b34246a78b5920852062ec223a75a3
Fl0piX <richar_5@epita.fr>
2017-03-15 16:54:41 +0100

    api

--- www/api.php
+++ www/api.php
@@ -3,12 +3,18 @@
 $pdo = new PDO('mysql:host=localhost;dbname=projetprogmobile;charset=utf8', 'root', '') or die('erreur de co');
 
 if(isset($_GET['idquestion'])) {
-    $req = $pdo->query("SELECT * FROM question WHERE id=".$_GET['id']);
+    $req = $pdo->query("SELECT * FROM question WHERE idquestion=".$_GET['idquestion']);
+    
+    $results = $req->fetchAll();
+    
+    echo json_encode($results, JSON_UNESCAPED_UNICODE);
 }
 
-$envoi = $pdo->prepare($req);
-$envoi->execute();
-
-echo json_encode($resultsid, JSON_UNESCAPED_UNICODE).json_encode($resultstitle, JSON_UNESCAPED_UNICODE);
+if(isset($_GET['user_score'])) {
+    $req = "UPDATE user SET user_score=".$_GET['user_score']." WHERE iduser=".$_GET["iduser"];
+    
+    $envoi = $pdo->prepare($req);
+    $envoi->execute();
+}
 
 ?>
